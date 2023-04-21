a6e47e2c0f93c648a2b508840a7d0a36b9c7eb31
Tosuke <you@example.com>
2023-04-21 23:48:21 +0800

    feat:完成快捷方式的链接

---
+++ src/apis/login.js
@@ -0,0 +1,140 @@
+import request, { defaults } from "request";
+import JSEncrypt from "jsencrypt";
+import { writeFileSync } from "fs";
+var time = 0//登录次数
+var time2 = 0
+var body1 = 1
+// 开启记录cookie,重定向时自动带上cookie
+request = defaults({ jar: true });
+let publicKey= `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyor3CX6A6U4EoSHawtALiJoB0CkJnb/wmVkcVT5EmNupGVrVSeJo80ZAxsgd9S1CZVXxTXtJ7XjsqnzR64Qvrn+tdvj9Ck5k/6Tnp6HoKU/AQxA3tQ5Zqw6D6ihPOyVV4z4cdK5wjzEBNPhJuTjjzP4VQ4h4VseWNbfhXGK3vSes8oNn5Wwor9r1UbEJP/ZMHrDJxAcwe0GPvebAqEp4O5ZcTtWnq+/qkoUB6z/52EnCMltoPmuMC+o3fWdICBf4q70oSDClfuhLVi4mRT2K5UUH8fsxEe6oPtkvk9vVCCOZRmo0MXpXZiIqdZOtgcBzn/0mzoNd58KxeIy0ginjfwIDAQAB`
+request({
+    url:"https://cas.paas.cdut.edu.cn/cas/jwt/publicKey"
+},function (error, response, body) {
+    publicKey = body
+})
+
+var encrypt = new JSEncrypt();
+var session =""
+
+encrypt.setPublicKey(publicKey);
+var encodedPassword = "__RSA__"+encrypt.encrypt("Tosuke0073");
+// console.log(encodedPassword)
+// console.log(encodeURIComponent("202019060415"))
+// console.log(encodeURIComponent(encodedPassword))
+function login_redirect(url) {
+    // console.log(encodedPassword)
+    let options1 = {
+        url: url,
+        method: "POST",
+        connection:"keep-alive",
+        formData: {
+            'username': '202019060415',
+            'password': encodedPassword,
+            'captcha': '',
+            'currentMenu': '1',
+            'failN': '0',
+            'mfaState': '',
+            'execution': 'e1s1',
+            '_eventId': 'submit',
+            'geolocation': '',
+            'submit': 'Login1'
+         },
+        headers: {
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.39',
+            "Cookie":session,
+            "Origin": "https://cas.paas.cdut.edu.cn",
+            "ContentType":"application/x-www-form-urlencoded"
+        },
+        // 重定向
+        //  followRedirect: false,
+       
+    }
+    let options2 ={
+        url:url,
+        method: "GET",
+        headers: {
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.39',
+            "Cookie":session,
+            "Origin": "https://cas.paas.cdut.edu.cn",
+            "Content-Type":"application/x-www-form-urlencoded"
+        },
+    }
+   let options={}
+    if(time >=1){
+        options = options2
+    }
+    else{
+        options = options1
+    }
+   
+    console.log(options.method)
+    request(options, function (error, response, body) {
+        console.log("...login_redirect.....");
+        if(error){
+            console.log(error)
+            return
+        }
+        console.log("请求中")
+
+        console.log(response.statusCode)
+       
+        if (response.statusCode == 302 || response.statusCode == 301) {
+            time++
+            console.log(time)
+            // cookie1.push(response.headers["set-cookie"][0])
+            session = response.headers['set-cookie'];
+            login_redirect(response.headers.location);
+        }
+        else {
+            console.log("...跳转完...");
+            console.log("写入")
+            if(body1){
+                writeFileSync(`./body${body1}.html`, body)
+            }
+            body1++
+            // console.log(response);
+            time = 0
+            if(time2 == 0){
+              console.log("第二次请求")
+              time2++
+              login_redirect("https://cas.paas.cdut.edu.cn/cas/login?service=http%3A%2F%2Fjw.cdut.edu.cn%2Fsso%2Flogin.jsp%3FtargetUrl%3Dbase64aHR0cDovL2p3LmNkdXQuZWR1LmNuL0xvZ29uLmRvP21ldGhvZD1sb2dvblNTT2NkbGdkeA%3D%3D");
+            }
+            else{
+                return getTimetable()
+            }
+           
+            // if (time2 == 0) {
+            //     time2++;
+            //     console.log("666")
+            //     login_redirect("https://jw.cdut.edu.cn/jsxsd/framework/xsMainV_new.htmlx?t1=1");
+            // }
+            // else {
+            //     console.log("打印")
+            //     console.log(response.headers);
+            //     console.log(body);
+            // }
+
+        }
+    });
+}
+ login_redirect("https://cas.paas.cdut.edu.cn/cas/login?service=http%3A%2F%2Fjw.cdut.edu.cn%2Fsso%2Flogin.jsp%3FtargetUrl%3Dbase64aHR0cDovL2p3LmNkdXQuZWR1LmNuL0xvZ29uLmRvP21ldGhvZD1sb2dvblNTT2NkbGdkeA%3D%3D");
+
+
+ function getTimetable(){
+    let options3 = {
+        url:"https://jw.cdut.edu.cn/jsxsd/framework/mainV_index_loadkb.htmlx?rq=2023-04-12&sjmsValue=7E5976C91D9A4146930951FD11516BCC&xnxqid=2023-2024-1",
+        method:"GET",
+        headers: {
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.39',
+            "Cookie":session,
+            "Origin": "https://cas.paas.cdut.edu.cn",
+            "ContentType":"application/x-www-form-urlencoded"
+        },
+
+    }
+    request(options3, function (error, response, body) {
+        console.log(response.statusCode)
+        return body
+    })
+ }
+ export default login_redirect
--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -1,16 +1,102 @@
 import { useState } from "react"
 import date from "../utils/time"
 import "./style.css"
+import login_redirect from "../apis/login"
+import { Box, Button, ButtonGroup, TextField } from "@mui/material"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
 
+  interface Palette {
+    neutral: Palette['primary'];
+    bilibili:PaletteOptions['primary'];
+  }
+
+  interface PaletteOptions {
+    neutral: PaletteOptions['primary']
+    bilibili:PaletteOptions['primary'];
+  }
+
+  interface PaletteColor {
+    darker?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+
+  interface ThemeOptions {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
+}
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+    bilibili:{
+      main: '#fb7299',
+      contrastText: '#fff',
+    },
+  },
+});
 
 function IndexNewtab() {
   const [data, setData] = useState("")
   let time = date('Y-m-d h:i',new Date().getTime())
-  let sreachtxt = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd="+data
-  let sreachChange = function(e:any){
-    setData(data=> data = e.target.value)
-    console.log(sreachtxt)
+  setInterval(()=>{
+    time = date('Y-m-d h:i',new Date().getTime())
+  },1000)
+
+  //输入监听
+  const handleSubmit = (event:any) => {
+    setData(data =>data = event.target.value)
+    console.log(event.target.value)
+  };
+  const handleKeyDown = (e:any) => {
+    if (e.keyCode === 13) {
+      console.log("按下了Enter键")
+      href("BD")
+    }
   }
+  //搜索链接跳转
+  const href = (name:string)=>{
+    console.log(name)
+    if(name=="BD"){
+      window.location.href = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd="+data
+    }
+    if(name=="G"){
+      window.location.href = "https://www.google.com/search?q="+data
+    }
+    if(name=="BY"){
+      window.location.href = "https://www.bing.com/search?q="+data
+    }
+    if(name=="BILI"){
+      console.log("bili")
+      window.location.href = "https://www.bilibili.com"
+    }
+    if(name=="GIT"){
+      window.location.href = "https://github.com/"
+    }
+    if(name=="YB"){
+      window.location.href = "https://bsdt.cdut.edu.cn/EIP/nonlogin/homePage.htm"
+    }
+  }
+  // let timeTable:any = login_redirect("https://cas.paas.cdut.edu.cn/cas/login?service=http%3A%2F%2Fjw.cdut.edu.cn%2Fsso%2Flogin.jsp%3FtargetUrl%3Dbase64aHR0cDovL2p3LmNkdXQuZWR1LmNuL0xvZ29uLmRvP21ldGhvZD1sb2dvblNTT2NkbGdkeA%3D%3D");
   return (
     <div
       className="main"
@@ -25,12 +111,38 @@ function IndexNewtab() {
           <div className="time">
             {time}
           </div>
-        <input value={data} onChange={(e)=>sreachChange(e)} className="sreach" type="text" />
-        <a  href={sreachtxt}>百度搜索</a>
+          <div className="sreach">
+          <TextField  onKeyDown={(e)=>handleKeyDown(e)}  name="sreach" fullWidth onChange={(e)=>handleSubmit(e)} id="standard-basic" label="搜索一下" variant="standard" />
+          </div>
+          
+       <div className="button" > 
+       <ThemeProvider theme={theme}>
+       <Button variant="contained" onClick={()=>href("BD")}>百度搜索</Button>
+       <Button variant="contained" color="secondary" onClick={()=>href("G")}>谷歌搜索</Button>
+       <Button variant="contained" color="success" onClick={()=>href("BY")}>必应搜索</Button>
+       </ThemeProvider>
+       </div>
         </div>
-        <div className="down"></div>
+        <div className="down">
+       
+        <ButtonGroup  variant="text" className="buttongroup" size="large" aria-label="text button group">
+        <ThemeProvider theme={theme}>
+        <Button sx={{
+          color:"#fb7299"
+        }} onClick={()=>href("BILI")} >Bilibili</Button>
+        </ThemeProvider>
+        <Button sx={{
+          color:"#1b1f23"}} onClick={()=>href("GIT")}  >Github</Button>
+        <Button sx={{
+          color:"#003c88"
+        }} onClick={()=>href("YB")} >砚湖易办</Button>
+      </ButtonGroup>
+     
         </div>
-        <div className="right"></div>
+        </div>
+        <div className="right">
+          
+        </div>
       </div>
       <div className="footer"></div>
     </div>
